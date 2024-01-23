5186c61301f135a5916185e1fbace639a6e07182
Elisha <elishakibet67@gmail.com>
2024-01-23 21:04:45 +0300

    Update the app.py

--- server/app.py
+++ server/app.py
@@ -1,1 +1,12 @@
-print("hello")
+from flask import Flask, request, make_response, jsonify
+from flask_restful import Resource, Api
+from flask_migrate import Migrate
+from models import db, Member, Route, Matatu
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fleets.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+api = Api(app)
