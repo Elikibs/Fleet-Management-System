98106eabde59e0b7cfe018eb181716e397e406c6
jimmindungu3 <jimmindungu3@gmail.com>
2024-01-25 09:32:00 +0300

    Create landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Herosection from "./components/Herosection";
+import "./App.css";
+
+function App() {
+  return (
+    <nav>
+      <Navbar />
+      <Herosection />
+    </nav>
+  );
+}
+
+export default App;
