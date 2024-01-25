6354104efcb404a01377ac414eb560b17009d92b
Tarbiee <dorcusstaby@gmail.com>
2024-01-25 21:38:41 +0300

    landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from "./Header";
 import Herosection from "./Herosection";
 
-export default function LangingPage() {
+export default function LandingPage() {
   return (
     <div className='landingpage'>
       <Header />
