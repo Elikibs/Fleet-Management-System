5b5e9ea7d820881327acdb8c771801a2dff29512
mararo-ops <danielmararo1@gmail.com>
2024-01-26 02:33:02 +0300

    home page changes

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -14,8 +14,7 @@ export default function Home() {
    {/* <h1>Home Page</h1> */}
    <div className='welcome'>
     <h1>Hello User</h1>
-    <h2>Revolutionize Your Fleet Management with <strong>Fleetsense</strong>.Your All-in-One Solution for Streamlined Operations. Experience Advanced Analytics, and Seamless Communication. Boost Efficiency, Cut Costs, and Drive Success. Join us Today for a Smarter Tomorrow!</h2>
-
+    <h2>Revolutionize Your Fleet Management with <strong>Fleetsense</strong>.</h2>
    </div>
    </div>
    </div>
