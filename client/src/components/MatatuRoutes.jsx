62dd651e6908f328d2fbf36bd576737bb5568d1a
Tarbiee <dorcusstaby@gmail.com>
2024-01-25 12:36:59 +0300

    add matatus components

--- client/src/components/MatatuRoutes.jsx
+++ client/src/components/MatatuRoutes.jsx
@@ -1,26 +1,42 @@
-import Login from './Login';
-import Register from './Register';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Members from './Members';
-import Matatus from './Matatus';
-import MatatuRoutes from './MatatuRoutes';
-import AddMatatu from './AddMatatu';
+import React from 'react'
+import NavBar from './NavBar'
+import Dashboard from './Dashboard'
+import Footer from './Footer'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function App() {
+
+export default function MatatuRoutes() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/members" element={<Members/>}/>
-        <Route path="/matatu_routes" element={<MatatuRoutes/>}/>
-        <Route path="/matatus" element={<Matatus/>}/>
-        <Route path="/add_matatu" element={<AddMatatu/>}/>
-      </Routes>
+    <div>
+       <NavBar/>
+      <div className='display'>
+      <Dashboard/>
+      <div className='content'>
+        <h1>Routes Page</h1>
+        <button className='button'>Add Route</button>
+        <ul>
+          <span>
+            <div className='lists'>
+              <h5>NRB-Juja</h5>
+              <h5 className='price'>Price:80</h5>
+              <span className='deleteroute'>
+              <FontAwesomeIcon icon={faTrashCan} style={{color:'navy'}}/>
+              </span>
+            </div>
+            <div className='lists'>
+              <h5>NRB-Kikuyu</h5>
+              <h5 className='price'>Price:70</h5>
+              <span className='deleteroute'>
+              <FontAwesomeIcon icon={faTrashCan} style={{color:'navy'}}/>
+              </span>
+            </div>
+          </span>
+        </ul>
+      </div>
+      </div>
+     <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
---
+++ client/src/components/Matatus.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Dashboard from './Dashboard'
+import Footer from './Footer'
+import { Table } from 'react-bootstrap'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
+
+export default function Matatus() {
+    const navigate= useNavigate()
+
+    function addMatatu(){
+        navigate("/add_matatu")
+    }
+  return (
+    <div>
+       <NavBar/>
+      <div className='display'>
+      <Dashboard/>
+      <div className='content'>
+      <h1>List of Matatus</h1>
+      <button className='buttons'onClick={addMatatu}>Add Matatu</button>
+      <br></br>
+      <br></br>
+      <Table responsive style={{width:'1400px', height:'400px'}}>
+          <thead>
+            <tr>
+              <th>Indentity</th>
+              <th>Capacity</th>
+              <th>Operation Route</th>
+              <th>Edit</th>
+              <th>Delete</th>  
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+                <td>KCB 490B</td>
+                <td>KCB 490B</td>
+                <td>KCB 490B</td>
+                <td>
+                  <FontAwesomeIcon icon={faPen} style={{color:'#40A2D8'}}/>
+                </td>
+                <td>
+                  <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}}/>
+                </td>
+            </tr>
+            <tr>
+                <td>KCB 490B</td>
+                <td>KCB 490B</td>
+                <td>KCB 490B</td>
+                <td>
+                  <FontAwesomeIcon icon={faPen} style={{color:'#40A2D8'}}/>
+                </td>
+                <td>
+                  <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}}/>
+                </td>
+            </tr>
+            <tr>
+               <td>KCB 490B</td>
+                <td>KCB 490B</td>
+                <td>KCB 490B</td>
+                <td>
+                  <FontAwesomeIcon icon={faPen} style={{color:'#40A2D8'}} />
+                </td>
+                <td>
+                  <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}}/>
+                </td>
+            </tr>
+            <tr>
+               <td>KCB 490B</td>
+                <td>KCB 490B</td>
+                <td>KCB 490B</td>
+                <td>
+                  <FontAwesomeIcon icon={faPen} style={{color:'#40A2D8'}}/>
+                </td>
+                <td>
+                  <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}}/>
+                </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+      </div>
+     <Footer/>
+    </div>
+  )
+}
