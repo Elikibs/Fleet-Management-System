25565bb5682ba1ab971196fa61068b6046cb5ee0
Tarbiee <dorcusstaby@gmail.com>
2024-01-25 12:29:53 +0300

    add members component

--- client/src/components/Members.jsx
+++ client/src/components/Members.jsx
@@ -1,2 +1,52 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Dashboard from './Dashboard'
+import Footer from './Footer'
+import { Card } from 'react-bootstrap'
+
+export default function Members() {
+  return (
+  <div>
+    <NavBar/>
+     <div className='display'>
+       <Dashboard/>
+     <div className='content'>
+      <h1>Members Page</h1>
+      <br></br>
+      <div className='cards' style={{gap:'30px'}}>
+      <Card style={{ width: '18rem',borderRadius:'20px' }}>
+      <Card.Body>
+      <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/9131/9131529.png" style={{height:'90px',width:'90px',display:'block', margin:'auto'}}/>
+      <br></br>
+        <Card.Subtitle className="mb-2 text-muted">Member name</Card.Subtitle>
+        <Card.Text>Member Description</Card.Text>
+        <Card.Link href="#">View Member</Card.Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem',borderRadius:'20px' }}>
+      <Card.Body>
+      <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/9131/9131529.png" style={{height:'90px',width:'90px',display:'block', margin:'auto'}}/>
+      <br></br>
+        <Card.Subtitle className="mb-2 text-muted">Member name</Card.Subtitle>
+        <Card.Text>Member Description</Card.Text>
+        <Card.Link href="#">View Member</Card.Link>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem',borderRadius:'20px' }}>
+      <Card.Body>
+      <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/9131/9131529.png" style={{height:'90px',width:'90px',display:'block', margin:'auto'}}/>
+      <br></br>
+        <Card.Subtitle className="mb-2 text-muted">Member name</Card.Subtitle>
+        <Card.Text>Member Description</Card.Text>
+        <Card.Link href="#">View Member</Card.Link>
+      </Card.Body>
+    </Card>
+      </div>
+      
+    
+     </div>
+    </div>
+     <Footer/>
+ </div>
+  )
+}
