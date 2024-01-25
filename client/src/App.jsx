523703dae6a911a8d1df19b9a18a711b645d64b1
Tarbiee <dorcusstaby@gmail.com>
2024-01-25 21:02:49 +0300

    component routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
-import Navbar from "./components/Navbar";
-import Herosection from "./components/Herosection";
+import { Route, Routes } from 'react-router-dom';
+import LangingPage from './LangingPage';
+import Login from './Login';
+import Register from './Register';
+import Home from './Home';
+import Members from './Members';
+import MatatuRoutes from './MatatuRoutes';
+import Matatus from './Matatus';
+import AddMatatu from './AddMatatu';
 import "./App.css";
 
 function App() {
   return (
-    <nav>
-      <Navbar />
-      <Herosection />
-    </nav>
+    <div className='App'>
+      <Routes>
+      <Route path="/" element={<LangingPage/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/members" element={<Members/>}/>
+      <Route path="/matatu_routes" element={<MatatuRoutes/>}/>
+      <Route path="/matatus" element={<Matatus/>}/>
+      <Route path="/add_matatu" element={<AddMatatu/>}/>
+
+      </Routes>
+
+    </div>
   );
 }
 
