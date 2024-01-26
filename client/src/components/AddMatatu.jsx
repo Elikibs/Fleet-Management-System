6e6a32c373be88b118512c18db9dad9cf49a8214
Tarbiee <dorcusstaby@gmail.com>
2024-01-26 17:53:49 +0300

    function to post matatus

--- client/src/components/AddMatatu.jsx
+++ client/src/components/AddMatatu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React, {useState} from 'react';
 import NavBar from './NavBar';
 import Dashboard from './Dashboard';
 import Footer from './Footer';
@@ -8,7 +8,52 @@ import { useNavigate } from 'react-router-dom';
 
 export default function AddMatatu() {
     const navigate= useNavigate()
-    function handleMatatu(){
+    // const[input, setInput] = useState({
+    //   name :'',
+    //   route:'',
+    //   driverscontact:'',
+    //   numberplate: '',
+    //   capacity:'',
+    //   trips:''
+    // })
+  //   const handleInput = (e)=>{
+  //     const { name, value } = e.target;
+  //     setInput({
+  //       ...input,
+  //       [name]: value
+  //     });
+
+  // }
+// handleAddMatatu
+    function handleMatatu(e){
+      // e.preventDefault();
+      // const item= {
+      //   name : input.name,
+      //   route: input.route,
+      //   driverscontact:input.driverscontact,
+      //   numberplate: input.numberplate,
+      //   capacity: input.capacity,
+      //   trips: input.trips,
+      // };
+      // fetch("/add_matatu" ,{
+      //   method: "POST",
+      //   headers:{
+      //     "Content-Type": "application/json"
+      //   },
+      //   body: JSON.stringify(item),
+      // })
+      // .then((r) => r.json())
+      // .then((newMatatu) => handleAddMatatu(newMatatu))
+      // setInput({
+      //   name :'',
+      // route:'',
+      // driverscontact:'',
+      // numberplate: '',
+      // capacity:'',
+      // trips:''
+
+      // })
+
        navigate("/matatus")
     }
   return (
@@ -27,31 +72,37 @@ export default function AddMatatu() {
             <FloatingLabel label="Name" className='mb-5'>
              <Form.Control type="text" style={{width:'90%'}}
              name='name'
+            //  onChange={handleInput}
              placeholder="Name" />
             </FloatingLabel>
             <FloatingLabel label="Route" className='mb-5'>
              <Form.Control type="text" style={{width:'90%'}}
              name='route'
+             //  onChange={handleInput}
              placeholder="Route" />
             </FloatingLabel>
             <FloatingLabel  label="Drivers contact" className='mb-5'>
              <Form.Control type="text" style={{width:'90%'}}
              name='driverscontact' 
+             //  onChange={handleInput}
              placeholder="Drivers contact" />
             </FloatingLabel>
             <FloatingLabel  label="Number Plate" className='mb-5'>
              <Form.Control type="text" style={{width:'90%'}}
-             name='numerplate'
+             name='numberplate'
+             //  onChange={handleInput}
              placeholder="Number Plate" />
             </FloatingLabel>
             <FloatingLabel  label="Capacity" className='mb-5'>
              <Form.Control type="text" style={{width:'90%'}}
              name='capacity' 
+             //  onChange={handleInput}
              placeholder="Capacity" />
             </FloatingLabel>
             <FloatingLabel  label="Trips" className='mb-5'>
              <Form.Control type="text" style={{width:'90%'}}
              name='trips' 
+             //  onChange={handleInput}
              placeholder="Trips" />
             </FloatingLabel>
             <Button variant="secondary" onClick={handleMatatu}style={{backgroundColor:"navy", marginBottom:"50px",marginLeft:'500px'}}>Add Matatu</Button>
