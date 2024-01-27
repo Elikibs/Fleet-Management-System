e31fc91e742825c63ded8ed9a1172ea57ccebfc7
Tarbiee <dorcusstaby@gmail.com>
2024-01-27 12:50:38 +0300

    edit matatu component

--- client/src/components/Matatus.jsx
+++ client/src/components/Matatus.jsx
@@ -1,40 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import NavBar from './NavBar'
 import Dashboard from './Dashboard'
-import { useState } from 'react'
 import Footer from './Footer'
 import { Table } from 'react-bootstrap'
 import { faPen } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
-import EditMatatu from './EditMatatu'
+
 
 //matatu prop
-export default function Matatus() {
+export default function Matatus({matatus, handleDeleteMatatu}) {
     const navigate= useNavigate()
-    const [modalShow, setModalShow] = useState(false);
-    const hideModal = () => setModalShow(false);
-    const toggleModal = () => setModalShow(!modalShow);
-    const handleAddRoute = (event) => {
-      event.preventDefault();
-      toggleModal();
-    };
-    const handleDeleteMatatu = (matatuId) => {
-      fetch(`/api/matatus/${matatuId}`, {
-          method: 'DELETE'
-      })
-      .then(response => {
-          if (response.ok) {
-              console.log('Matatu deleted successfully');
-          } else {
-              console.error('Failed to delete matatu');
-          }
-      })
-      .catch(error => {
-          console.error('Error deleting matatu:', error);
-      });
-  };
 
     function addMatatu(){
         navigate("/add_matatu")
@@ -60,55 +38,26 @@ export default function Matatus() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-                <td>KCB 490B</td>
-                <td>KCB 490B</td>
-                <td>KCB 490B</td>
-                <td>
-                  <FontAwesomeIcon icon={faPen} style={{color:'#40A2D8'}} onClick={toggleModal}/>
-                </td>
-                <td>
-                  <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}} onClick={handleDeleteMatatu}/>
-                </td>
-            </tr>
-            <tr>
-                <td>KCB 490B</td>
-                <td>KCB 490B</td>
-                <td>KCB 490B</td>
-                <td>
-                  <FontAwesomeIcon icon={faPen} style={{color:'#40A2D8'}}onClick={toggleModal}/>
-                </td>
-                <td>
-                  <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}} onClick={handleDeleteMatatu}/>
-                </td>
-            </tr>
-            <tr>
-               <td>KCB 490B</td>
-                <td>KCB 490B</td>
-                <td>KCB 490B</td>
-                <td>
-                  <FontAwesomeIcon icon={faPen} style={{color:'#40A2D8'}} onClick={toggleModal} />
-                </td>
-                <td>
-                  <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}} onClick={handleDeleteMatatu}/>
-                </td>
-            </tr>
-            <tr>
-               <td>KCB 490B</td>
-                <td>KCB 490B</td>
-                <td>KCB 490B</td>
-                <td>
-                  <FontAwesomeIcon icon={faPen} style={{color:'#40A2D8'}} onClick={toggleModal}/>
-                </td>
-                <td>
-                  <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}} onClick={handleDeleteMatatu}/>
-                </td>
-            </tr>
+            {matatus.map((matatu) =>(
+               <tr key={matatu.id}>
+               <td>{matatu.number_plate}</td>
+               <td>{matatu.capacity}</td>
+               <td>{matatu.route_id}</td>
+               <td>
+               <Link to={`/edit_matatu/${matatu.id}`}>
+                        <FontAwesomeIcon icon={faPen} style={{ color: '#40A2D8' }} />
+                </Link>
+               </td>
+               <td>
+                 <FontAwesomeIcon icon={faTrashCan} style={{color:'#40A2D8'}} onClick={() => handleDeleteMatatu(matatu.id)}/>
+               </td>
+           </tr>
+
+            ))}
           </tbody>
         </Table>
       </div>
       </div>
-      <EditMatatu show={modalShow} onHide={hideModal} onAddRoute={handleAddRoute} />
      <Footer/>
     </div>
   )
